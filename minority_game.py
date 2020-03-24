ff8a721594f171d00db34f8832ac566dd4ea6ade
ruihan0495 <34216537+ruihan0495@users.noreply.github.com>
2020-03-24 15:48:32 +0100

    Customized environment

--- minority_game.py
+++ minority_game.py
@@ -1,8 +1,67 @@
 import numpy as np
 
+import gym
+from gym import spaces
+from gym.utils import seeding
+
 '''
 A game engine for minority game:
 step - a function called on the environment and return the next state, reward
 reward - a function calculate reward for agents
 state - a function generates the current state for agent
 '''
+class MinorGameEnv(gym.Env):
+    def __init__(self, k):
+        self.k = k
+        self.num_agents = 2*k+1
+        self.action_space = spaces.Discrete(2)
+        self.observation_space = spaces.Box(low=0, high=1,
+                                 shape=(self.num_agents-1,2),
+                                 dtype=np.float32)
+        self.action_list = np.array([0] * self.num_agents)
+        self.reward_list = np.array([0] * self.num_agents)
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def set_reward(self, agent_id):
+        agent_action = self.action_list[agent_id]
+        num_same_action = np.count_nonzero(self.action_list==agent_action)
+        if num_same_action <= self.k:
+            self.reward_list[agent_id] = 1
+        else:
+            self.reward_list[agent_id] = 0
+
+    def get_observation(self, agent_id):
+        obs = np.zeros((self.num_agents-1,2),dtype=np.float32)
+        index = 0
+        for i in range(self.num_agents):
+            if i != agent_id:
+                obs[index,:] = [self.action_list[i],self.reward_list[i]]
+                index += 1 
+        return obs
+
+    def step(self, action_n):
+        total_obs = np.zeros((self.num_agents, self.num_agents-1, 2), dtype=np.float32)
+        # update actions together
+        for agent_id in range(self.num_agents):
+            self.action_list[agent_id] = action_n[agent_id]
+        # update rewards together    
+        for agent_id in range(self.num_agents):    
+            self.set_reward(agent_id)
+        # concatenate all observations
+        for agent_id in range(self.num_agents):    
+            next_observation = self.get_observation(agent_id)
+            total_obs[agent_id,:,:] = next_observation
+        reward_list = self.reward_list
+        return reward_list, total_obs
+
+    def reset(self):
+        self.action_list = np.array([0] * self.num_agents)
+        self.reward_list = np.array([0] * self.num_agents)
+
+    def render(self):
+        # print the action and reward list at current stage
+        print("Current actions are: ", self.action_list)
+        print("Current rewards are: ", self.reward_list)
